6b27f4985155189b2770faf7c796a622f9701178
underlineit-dev-5 <104749172+underlineit-dev-5@users.noreply.github.com>
2024-01-05 21:03:56 +0530

    modified

--- common/ScrollToUp.js
+++ common/ScrollToUp.js
@@ -1,20 +1,19 @@
 "use client";
 import React from "react";
-// import ScrollUpButton from "react-scroll-up-button";
+import ScrollUpButton from "react-scroll-up-button";
 
 const ScrollToUp = () => {
   return (
-    <></>
-      //   <ScrollUpButton
-      //   StopPosition={0}
-      //   ShowAtPosition={300}
-      //   EasingType="easeOutCubic"
-      //   AnimationDuration={500}
-      //   ContainerClassName="ScrollUpButton"
-      //   TransitionClassName="ScrollUpButton__Toggled"
-      //   style={{}}
-      //   ToggledStyle={{}}
-      // />
+        <ScrollUpButton
+        StopPosition={0}
+        ShowAtPosition={300}
+        EasingType="easeOutCubic"
+        AnimationDuration={500}
+        ContainerClassName="ScrollUpButton"
+        TransitionClassName="ScrollUpButton__Toggled"
+        style={{}}
+        ToggledStyle={{}}
+      />
   );
 };
 
