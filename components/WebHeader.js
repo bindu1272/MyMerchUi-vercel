634ec9146a1ba1f0a6fa9a9175258f991cd2e9e1
underlineit-dev-5 <104749172+underlineit-dev-5@users.noreply.github.com>
2024-03-20 22:36:14 +0530

    fix : fixed issue

--- components/WebHeader.js
+++ components/WebHeader.js
@@ -69,128 +69,127 @@ const WebHeader = ({
   }, [])
 
   return (
-    <h1>hai</h1>
-    // isClient ?
-    //   <>
-    //     <BulkOrder
-    //       showBulkOrder={showBulkOrder}
-    //       setShowBulkOrder={setShowBulkOrder}
-    //     />
-    //     <Login
-    //       showLogin={showLogin}
-    //       setShowLogin={setShowLogin}
-    //       setShowSignUp={setShowSignUp}
-    //       redirect={redirect}
-    //     />
-    //     <Registration
-    //       showSignUp={showSignUp}
-    //       setShowSignUp={setShowSignUp}
-    //     />
-    //     <div className="top-header">
-    //       <div className="container">
-    //         <div className="row">
-    //           <div className="col-sm-5 d-flex align-items-center">
-    //             <Link href="callto:1800959308" className="call-link">
-    //               <PhoneOutlined className="mr-3" /><strong> 1800 959 308</strong>
-    //             </Link>
-    //             <span style={{ color: "white", marginLeft: 15, marginRight: 15 }}></span>
-    //             <Link className="call-link" style={{ color: "white" }} href="/GetAQuote">
-    //               Get a Quote
-    //             </Link>
-    //             {/* <span style={{ color: "white", marginLeft: 15, marginRight: 15 }}>|</span>
-    //             <Link href="" className="call-link" style={{ color: "white" }} onClick={() => setShowBulkOrder(true)}>
-    //               Bulk Enquiry
-    //             </Link> */}
-    //           </div>
-    //           <div className="col-sm-7">
-    //             <div className="d-flex align-items-center justify-content-end">
-    //               <Input
-    //                 placeholder="Search"
-    //                 prefix={<SearchOutlined />}
-    //                 value={searchText}
-    //                 onChange={e => {
-    //                   setSearchText(e.target.value)
-    //                   store.dispatch({
-    //                     type: TYPES.SET_ENQUIRY_PRODUCTS_SEARCH_STRING,
-    //                     payload: e.target.value
-    //                   });
-    //                 }}
-    //                 onKeyDown={e => {
-    //                   if (e.key === 'Enter') {
-    //                     handleSearch();
-    //                   }
-    //                 }}
+    isClient ?
+      <>
+        <BulkOrder
+          showBulkOrder={showBulkOrder}
+          setShowBulkOrder={setShowBulkOrder}
+        />
+        <Login
+          showLogin={showLogin}
+          setShowLogin={setShowLogin}
+          setShowSignUp={setShowSignUp}
+          redirect={redirect}
+        />
+        <Registration
+          showSignUp={showSignUp}
+          setShowSignUp={setShowSignUp}
+        />
+        <div className="top-header">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-5 d-flex align-items-center">
+                <Link href="callto:1800959308" className="call-link">
+                  <PhoneOutlined className="mr-3" /><strong> 1800 959 308</strong>
+                </Link>
+                <span style={{ color: "white", marginLeft: 15, marginRight: 15 }}></span>
+                <Link className="call-link" style={{ color: "white" }} href="/GetAQuote">
+                  Get a Quote
+                </Link>
+                {/* <span style={{ color: "white", marginLeft: 15, marginRight: 15 }}>|</span>
+                <Link href="" className="call-link" style={{ color: "white" }} onClick={() => setShowBulkOrder(true)}>
+                  Bulk Enquiry
+                </Link> */}
+              </div>
+              <div className="col-sm-7">
+                <div className="d-flex align-items-center justify-content-end">
+                  <Input
+                    placeholder="Search"
+                    prefix={<SearchOutlined />}
+                    value={searchText}
+                    onChange={e => {
+                      setSearchText(e.target.value)
+                      store.dispatch({
+                        type: TYPES.SET_ENQUIRY_PRODUCTS_SEARCH_STRING,
+                        payload: e.target.value
+                      });
+                    }}
+                    onKeyDown={e => {
+                      if (e.key === 'Enter') {
+                        handleSearch();
+                      }
+                    }}
                     
-    //                 // onKeyPress={e => {
-    //                 //   if (e.key === 'Enter') {
-    //                 //     handleSearch()
-    //                 //   }
-    //                 // }}
-    //               />
-    //               <div className="avatar-block">
-    //                 {!userId ? (
-    //                   <>
-    //                     <a onClick={() => setShowLogin(true)}>Login</a>
-    //                     <a onClick={() => setShowSignUp(true)}>Sign Up</a>
-    //                   </>
-    //                 ) : (
-    //                   <>
-    //                     {userRoles && userRoles.includes("Admin") ? <a href="/admin" className="call-link">Admin</a> : ""}
-    //                     <div className="avatar_drop_down">
-    //                       <Dropdown
-    //                         overlay={menu(onLogout)}
-    //                         trigger={["click"]}
-    //                         overlayStyle={{ zIndex: 1000000 }}
-    //                         overlayClassName="avatar_select_menu"
-    //                       >
-    //                         <Link href=""
-    //                           className="ant-dropdown-link"
-    //                           onClick={(e) => e.preventDefault()}
-    //                           style={{ textDecoration: "none" }}
-    //                         >
-    //                           <UserOutlined className="avatar" />
-    //                           {userName} <DownOutlined />
-    //                         </Link>
-    //                       </Dropdown>
-    //                     </div>
-    //                   </>
-    //                 )}
-    //                 <a onClick={onClickCart} className="cart-items">
-    //                   <span className="d-flex align-items-center">
-    //                     <ShoppingCartOutlined />{" "}
-    //                     <span className="cart-count">
-    //                       {cart &&
-    //                         cart.products &&
-    //                         cart.products.length}
-    //                     </span>
-    //                   </span>
-    //                 </a>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <header>
-    //       <div className="header">
-    //         <div className="container">
-    //           <div className="row">
-    //             <div className="col-sm-7 brand">
-    //               <Link href="/">
-    //                 <S3Image src={"/mymerch-logo-black-desktop.svg"} width={150} height={52} />
-    //               </Link>
-    //             </div>
-    //             <div className="col-sm-5 position-static">
-    //               <WebHeaderNavigation
-    //                 navigationOptions={navigationOptions}
-    //               />
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </header>
-    //   </>
-    //   : null
+                    // onKeyPress={e => {
+                    //   if (e.key === 'Enter') {
+                    //     handleSearch()
+                    //   }
+                    // }}
+                  />
+                  <div className="avatar-block">
+                    {!userId ? (
+                      <>
+                        <a onClick={() => setShowLogin(true)}>Login</a>
+                        <a onClick={() => setShowSignUp(true)}>Sign Up</a>
+                      </>
+                    ) : (
+                      <>
+                        {userRoles && userRoles.includes("Admin") ? <a href="/admin" className="call-link">Admin</a> : ""}
+                        <div className="avatar_drop_down">
+                          <Dropdown
+                            overlay={menu(onLogout)}
+                            trigger={["click"]}
+                            overlayStyle={{ zIndex: 1000000 }}
+                            overlayClassName="avatar_select_menu"
+                          >
+                            <Link href=""
+                              className="ant-dropdown-link"
+                              onClick={(e) => e.preventDefault()}
+                              style={{ textDecoration: "none" }}
+                            >
+                              <UserOutlined className="avatar" />
+                              {userName} <DownOutlined />
+                            </Link>
+                          </Dropdown>
+                        </div>
+                      </>
+                    )}
+                    <a onClick={onClickCart} className="cart-items">
+                      <span className="d-flex align-items-center">
+                        <ShoppingCartOutlined />{" "}
+                        <span className="cart-count">
+                          {cart &&
+                            cart.products &&
+                            cart.products.length}
+                        </span>
+                      </span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <header>
+          <div className="header">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-7 brand">
+                  <Link href="/">
+                    <S3Image src={"/mymerch-logo-black-desktop.svg"} width={150} height={52} />
+                  </Link>
+                </div>
+                <div className="col-sm-5 position-static">
+                  <WebHeaderNavigation
+                    navigationOptions={navigationOptions}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </>
+      : null
   );
 };
 
