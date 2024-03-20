dca6b16186d7f461f9946a245b5a4cab749be4a3
underlineit-dev-5 <104749172+underlineit-dev-5@users.noreply.github.com>
2024-03-20 21:10:16 +0530

    fix : fixed issue

--- next.config.js
+++ next.config.js
@@ -1,29 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    publicRuntimeConfig: {
-      NEXT_APP_IMAGES_SRC_URL : "https://dev-assets.mymerch.com.au/src/images"
-        // Will be available on both server and client
-      },
-      images: {
-        domains: ['dev-assets.mymerch.com.au',"mymerch-strapi-staging.s3.ap-southeast-2.amazonaws.com","assets.mymerch.com.au","mymerch-strapi-live.s3.ap-southeast-2.amazonaws.com"],
-      },
-      env:{
-        NEXT_PUBLIC_STRAPI_API_URL : process.env.NEXT_PUBLIC_STRAPI_API_URL,
-        // REACT_APP_IMAGES_SRC_URL:process.env.REACT_APP_IMAGES_SRC_URL,
-        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-        NEXT_APP_GA_ID : process.env.NEXT_APP_GA_ID,
-        GATSBY_API_URL : process.env.GATSBY_API_URL,
-        NEXT_APP_IMAGES_SRC_URL: process.env.NEXT_APP_IMAGES_SRC_URL,
-        NEXT_JS_SITE_URL:process.env.NEXT_JS_SITE_URL
-      },
-      reactStrictMode: false,
-      typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
+  publicRuntimeConfig: {
+    NEXT_APP_IMAGES_SRC_URL : "https://dev-assets.mymerch.com.au/src/images"
+      // Will be available on both server and client
     },
+    images: {
+      domains: ['dev-assets.mymerch.com.au',"mymerch-strapi-staging.s3.ap-southeast-2.amazonaws.com","assets.mymerch.com.au","mymerch-strapi-live.s3.ap-southeast-2.amazonaws.com"],
+    },
+    env:{
+      NEXT_PUBLIC_STRAPI_API_URL : process.env.NEXT_PUBLIC_STRAPI_API_URL,
+      // REACT_APP_IMAGES_SRC_URL:process.env.REACT_APP_IMAGES_SRC_URL,
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+      NEXT_APP_GA_ID : process.env.NEXT_APP_GA_ID,
+      GATSBY_API_URL : process.env.GATSBY_API_URL,
+      NEXT_APP_IMAGES_SRC_URL: process.env.NEXT_APP_IMAGES_SRC_URL,
+      NEXT_JS_SITE_URL:process.env.NEXT_JS_SITE_URL
+    },
+    reactStrictMode: false,
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
