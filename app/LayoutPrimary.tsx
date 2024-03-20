71aa535c451e4e5c053e8246446d393b16b24543
underlineit-dev-5 <104749172+underlineit-dev-5@users.noreply.github.com>
2024-03-20 21:40:38 +0530

    fix : fixed

--- app/LayoutPrimary.tsx
+++ app/LayoutPrimary.tsx
@@ -2,8 +2,8 @@
 import ReduxProvider from "./provider";
 import Header from "@/common/Header";
 import React, { useEffect } from "react";
-import TagManager from 'react-gtm-module';
-import { initGA } from '../utilities/GoogleSetUp';
+// import TagManager from 'react-gtm-module';
+// import { initGA } from '../utilities/GoogleSetUp';
 
 
 export default function LayoutPrimary({
@@ -11,14 +11,14 @@ export default function LayoutPrimary({
   }: {
     children: React.ReactNode
   }) {
-    useEffect(() => {
-      initGA(process.env.NEXT_PUBLIC_GA_ID);
-      TagManager.initialize({
-        gtmId: process.env.NEXT_PUBLIC_GTM_ID,
-        auth: process.env.NEXT_PUBLIC_GTM_AUTH,
-        preview: process.env.NEXT_PUBLIC_GTM_PREVIEW,
-      });
-    }, [])
+    // useEffect(() => {
+    //   initGA(process.env.NEXT_PUBLIC_GA_ID);
+    //   TagManager.initialize({
+    //     gtmId: process.env.NEXT_PUBLIC_GTM_ID,
+    //     auth: process.env.NEXT_PUBLIC_GTM_AUTH,
+    //     preview: process.env.NEXT_PUBLIC_GTM_PREVIEW,
+    //   });
+    // }, [])
     return (
       <ReduxProvider>
         <Header/>
