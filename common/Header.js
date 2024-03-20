ad43a363a58eba38381032c5821060d66646c916
underlineit-dev-5 <104749172+underlineit-dev-5@users.noreply.github.com>
2024-03-20 22:02:07 +0530

    fixed

--- common/Header.js
+++ common/Header.js
@@ -1,84 +1,84 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from 'next/navigation';
-import { connect, useDispatch } from "react-redux";
+// import { connect, useDispatch } from "react-redux";
 import { logoutRequest } from "@/actions/authActions";
 import {
   getUserId,
   getUserName,
   getUserRoles,
 } from "@/selectors/authSelector";
-import { getQueryParams } from "@/utilities/helpers";
-import MobileHeader from "@/components/MobileHeader";
-import WebHeader from "@/components/WebHeader";
-import MessageBox from "./MessageBox";
-import { HEADER_NAV_ITEMS } from "@/constants/appConstants";
+// import { getQueryParams } from "@/utilities/helpers";
+// import MobileHeader from "@/components/MobileHeader";
+// import WebHeader from "@/components/WebHeader";
+// import MessageBox from "./MessageBox";
+// import { HEADER_NAV_ITEMS } from "@/constants/appConstants";
 import { getCart } from "@/selectors/cartSelector";
 import { useSearchParams } from "next/navigation";
 import { getEnquiryProductsSearchString } from "@/selectors/enquiryProductSelector";
 
 const Header = ({
-  cart,
-  userId,
-  userName,
-  userRoles,
-  searchTextCache
+  // cart,
+  // userId,
+  // userName,
+  // userRoles,
+  // searchTextCache
 }) => {
-  const params = useSearchParams();
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const [showSignUp, setShowSignUp] = useState(false);
-  const [showLogin, setShowLogin] = useState(false);
-  const [toggleSidebar, setToggleSidebar] = useState(false);
-  const [searchText, setSearchText] = useState(searchTextCache);
-  const [showBulkOrder, setShowBulkOrder] = useState(false);
-  const [redirect, setRedirect] = useState();
-  const [showMobileSearch, setShowMobileSearch] = useState(false);
-  const navigationOptions = [...HEADER_NAV_ITEMS];
-  useEffect(() => {
-    if (!userId) {
-      const queryParams = getQueryParams(window.location.href);
-      if (queryParams.showLogin && queryParams.showLogin == "true") {
-        setShowLogin(true);
-      }
-      if (queryParams.redirect) {
-        setRedirect(queryParams.redirect);
-      }
-    }
-  }, []);
+  // const params = useSearchParams();
+  // const dispatch = useDispatch();
+  // const router = useRouter();
+  // const [showSignUp, setShowSignUp] = useState(false);
+  // const [showLogin, setShowLogin] = useState(false);
+  // const [toggleSidebar, setToggleSidebar] = useState(false);
+  // const [searchText, setSearchText] = useState(searchTextCache);
+  // const [showBulkOrder, setShowBulkOrder] = useState(false);
+  // const [redirect, setRedirect] = useState();
+  // const [showMobileSearch, setShowMobileSearch] = useState(false);
+  // const navigationOptions = [...HEADER_NAV_ITEMS];
+  // useEffect(() => {
+  //   if (!userId) {
+  //     const queryParams = getQueryParams(window.location.href);
+  //     if (queryParams.showLogin && queryParams.showLogin == "true") {
+  //       setShowLogin(true);
+  //     }
+  //     if (queryParams.redirect) {
+  //       setRedirect(queryParams.redirect);
+  //     }
+  //   }
+  // }, []);
 
-  function onShowMobileSearch() {
-    setShowMobileSearch(true);
-  }
+  // function onShowMobileSearch() {
+  //   setShowMobileSearch(true);
+  // }
 
-  function onCloseMobileSearch() {
-    setShowMobileSearch(false);
-  }
+  // function onCloseMobileSearch() {
+  //   setShowMobileSearch(false);
+  // }
 
-  const handleToggleSidebar = (value) => {
-    setToggleSidebar(value);
-  };
+  // const handleToggleSidebar = (value) => {
+  //   setToggleSidebar(value);
+  // };
 
-  const onLogout = () => {
-    dispatch(logoutRequest());
-    router.push(`/`);
-    // window.location.reload();
-  };
+  // const onLogout = () => {
+  //   dispatch(logoutRequest());
+  //   router.push(`/`);
+  //   // window.location.reload();
+  // };
 
-  const onClickTitle = (itemUrl) => {
-    router.push(itemUrl);
-  };
+  // const onClickTitle = (itemUrl) => {
+  //   router.push(itemUrl);
+  // };
 
-  const handleSearch = () => {
-    const searchUrl = searchText
-      ? `/AllMerch?searchString=${searchText}`
-      : `/AllMerch`;
-    router.push(searchUrl);
-  };
+  // const handleSearch = () => {
+  //   const searchUrl = searchText
+  //     ? `/AllMerch?searchString=${searchText}`
+  //     : `/AllMerch`;
+  //   router.push(searchUrl);
+  // };
 
-  const onClickCart = () => {
-    router.push("/SubmitEnquiry");
-  }
+  // const onClickCart = () => {
+  //   router.push("/SubmitEnquiry");
+  // }
 
   return (
     <h1>hello</h1>
@@ -131,14 +131,14 @@ const Header = ({
   );
 };
 
-function mapStateToProps(state) {
-  return {
-    cart: getCart(state),
-    userId: getUserId(state),
-    userName: getUserName(state),
-    userRoles: getUserRoles(state),
-    searchTextCache: getEnquiryProductsSearchString(state)
-  };
-}
-export default connect(mapStateToProps, {})(Header);
-// export default Header;
+// function mapStateToProps(state) {
+//   return {
+//     cart: getCart(state),
+//     userId: getUserId(state),
+//     userName: getUserName(state),
+//     userRoles: getUserRoles(state),
+//     searchTextCache: getEnquiryProductsSearchString(state)
+//   };
+// }
+// export default connect(mapStateToProps, {})(Header);
+export default Header;
